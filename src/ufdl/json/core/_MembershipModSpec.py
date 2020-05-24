0fc0d5814b5960d0c984c9b5009f431fc9522c0f
csterling <coreytsterling@gmail.com>
2020-05-25 11:11:29 +1200

    Added a mod-spec for team memberships.

---
+++ src/ufdl/json/core/_MembershipModSpec.py
@@ -0,0 +1,17 @@
+from wai.json.object import StrictJSONObject
+from wai.json.object.property import StringProperty, EnumProperty
+
+
+class MembershipModSpec(StrictJSONObject['MembershipModSpec']):
+    """
+    Message to modify the memberships of a team.
+    """
+    # The username of the membership to modify
+    username: str = StringProperty(min_length=1)
+
+    # The type of modification to make
+    method: str = EnumProperty(values=("add", "remove", "update"))
+
+    # The permissions to give the member (only required for 'add' and 'update',
+    # defaults to read-only permission if not specified).
+    permissions: str = EnumProperty(values=("R", "W", "A"), optional=True, default="R")
---
+++ src/ufdl/json/core/__init__.py
@@ -0,0 +1,1 @@
+from ._MembershipModSpec import MembershipModSpec
