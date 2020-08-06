dd2c85dcaa0a5217d83c142c2c5afe340de84043
csterling <coreytsterling@gmail.com>
2020-08-06 16:00:36 +1200

    Added specs for modifying inputs/parameters to job-templates.

---
+++ src/ufdl/json/core/jobs/_ParameterSpec.py
@@ -0,0 +1,14 @@
+from wai.json.object import StrictJSONObject
+from wai.json.object.property import StringProperty
+
+
+class ParameterSpec(StrictJSONObject['ParameterSpec']):
+    """
+    Specifies the JSON data structure for specifying the type
+    and default value to a parameter to a job template.
+    """
+    # The type of the input
+    type: str = StringProperty(max_length=32)
+
+    # The options to the input
+    default: str = StringProperty()
---
+++ src/ufdl/json/core/jobs/__init__.py
@@ -0,0 +1,5 @@
+"""
+Package specifying JSON messages relating to jobs.
+"""
+from ._InputSpec import InputSpec
+from ._ParameterSpec import ParameterSpec
