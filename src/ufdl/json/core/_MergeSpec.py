254be7e080cffee1b56a5f2ba31b1e4e412ff1b0
csterling <coreytsterling@gmail.com>
2020-08-21 13:43:31 +1200

    Added JSON object for specifiying parameters of merge operation.

---
+++ src/ufdl/json/core/_MergeSpec.py
@@ -0,0 +1,10 @@
+from wai.json.object import StrictJSONObject
+from wai.json.object.property import BoolProperty
+
+
+class MergeSpec(StrictJSONObject['MergeSpec']):
+    """
+    Message to merge an object into another.
+    """
+    # Whether to delete the source object after merging
+    delete: bool = BoolProperty()
--- src/ufdl/json/core/__init__.py
+++ src/ufdl/json/core/__init__.py
@@ -1,3 +1,4 @@
 from ._FileMetadata import FileMetadata
 from ._LicenceSubdescriptorViewSet import LicenceSubdescriptorModSpec
 from ._MembershipModSpec import MembershipModSpec
+from ._MergeSpec import MergeSpec
