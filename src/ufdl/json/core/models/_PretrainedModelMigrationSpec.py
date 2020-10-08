176063af0f162d1f47cd7f9671e48b837af6ca51
csterling <coreytsterling@gmail.com>
2020-10-08 15:23:18 +1300

    Added JSON migration specification for pre-trained models.

---
+++ src/ufdl/json/core/models/_PretrainedModelMigrationSpec.py
@@ -0,0 +1,17 @@
+from wai.json.object import JSONObject
+from wai.json.object.property import StringProperty
+
+
+class PretrainedModelMigrationSpec(JSONObject['PretrainedModelMigrationSpec']):
+    """
+    Represents the JSON structure used to specify a pre-trained model in
+    the migration of the database.
+    """
+    name = StringProperty(min_length=1, max_length=200)
+    description = StringProperty(max_length=200)
+    url = StringProperty(min_length=1, max_length=200)
+    licence = StringProperty(min_length=1, max_length=100)
+    framework = StringProperty(min_length=3, max_length=49)
+    domain = StringProperty(min_length=1, max_length=2)
+    source = StringProperty()
+    metadata = JSONObject.as_property()
---
+++ src/ufdl/json/core/models/__init__.py
@@ -0,0 +1,1 @@
+from ._PretrainedModelMigrationSpec import PretrainedModelMigrationSpec
